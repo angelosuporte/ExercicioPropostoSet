3e3100b02b16f933c8234886931594097b88a1e0
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-11 14:57:56 -0300

    Finaliza e testado exercício - ok

--- ExercicioPropostoSet/Program.cs
+++ ExercicioPropostoSet/Program.cs
@@ -8,24 +8,37 @@ namespace ExercicioPropostoSet
         static void Main(string[] args)
         {
             HashSet<int> A = new HashSet<int>();
-            Console.Write("Informe quantos alunos estão matriculados no curso Algoritmo: ");
+            Console.Write("Quantos alunos estão matriculados no curso Algoritmo: ");
             int algoritmo = int.Parse(Console.ReadLine());
             for ( int i = 0; i < algoritmo; i++)
             {
                 A.Add(int.Parse(Console.ReadLine()));
             }
-            ImprimeCurso(A);
-            Console.ReadKey();
 
-        }
-
-        static void ImprimeCurso<T>(IEnumerable<T> curso)
-        {
-            foreach (T aluno in curso)
+            HashSet<int> B = new HashSet<int>();
+            Console.Write("Quantos alunos estão matriculados no curso Business Intelligense: ");
+            int business = int.Parse(Console.ReadLine());
+            for (int i = 0; i < business; i++)
             {
-                Console.Write(aluno + " ");
+                B.Add(int.Parse(Console.ReadLine()));
             }
+
+            HashSet<int> C = new HashSet<int>();
+            Console.Write("Quantos alunos estão matriculados no curso ASP . NET Core MVC: ");
+            int core = int.Parse(Console.ReadLine());
+            for (int i = 0; i < core; i++)
+            {
+                C.Add(int.Parse(Console.ReadLine()));
+            }
+
             Console.WriteLine();
+        
+            HashSet<int> total = new HashSet<int>(A);
+            total.UnionWith(B);
+            total.UnionWith(C);
+            Console.WriteLine("Total de alunos: " + total.Count);
+
+            Console.ReadKey();
         }
     }
 }
